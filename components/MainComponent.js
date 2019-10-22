0738ea3945f006fb7f892e44b303805e8404dc78
AnikHawk <altamashaqueanik@gmail.com>
2019-10-23 01:41:01 +0600

    FavoriteCompoment

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -20,6 +20,7 @@ import {
   fetchLeaders,
 } from '../redux/ActionCreators'
 import Reservation from './ReservationComponent'
+import Favorites from './FavoriteComponent'
 
 const mapStateToProps = state => {
   return {
@@ -143,6 +144,31 @@ const ContactNavigator = createStackNavigator(
   }
 )
 
+const FavoritesNavigator = createStackNavigator(
+  {
+    Favorites: {screen: Favorites},
+  },
+  {
+    navigationOptions: ({navigation}) => ({
+      headerStyle: {
+        backgroundColor: '#512DA8',
+      },
+      headerTitleStyle: {
+        color: '#fff',
+      },
+      headerTintColor: '#fff',
+      headerLeft: (
+        <Icon
+          name="menu"
+          size={24}
+          iconStyle={{color: 'white'}}
+          onPress={() => navigation.toggleDrawer()}
+        />
+      ),
+    }),
+  }
+)
+
 const ReservationNavigator = createStackNavigator(
   {
     Reservation: {screen: Reservation},
@@ -242,6 +268,22 @@ const MainNavigator = createDrawerNavigator(
             type="font-awesome"
             size={22}
             color={tintColor}
+          />
+        ),
+      },
+    },
+
+    Favorites: {
+      screen: FavoritesNavigator,
+      navigationOptions: {
+        title: 'My Favorites',
+        drawerLabel: 'My Favorites',
+        drawerIcon: ({tintColor, focused}) => (
+          <Icon
+            name="heart"
+            type="font-awesome"
+            size={24}
+            iconStyle={{color: tintColor}}
           />
         ),
       },
